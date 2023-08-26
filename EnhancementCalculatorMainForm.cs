413cce2b790e88f71ff6f96df60c64e6fd2290ff
JosiahGraham <jogr9892@colorado.edu>
2023-08-26 00:12:16 -0600

    Added items to Item Name combobox (not completed, mostly only yellow and orange grade added)

--- EnhancementCalculatorMainForm.cs
+++ EnhancementCalculatorMainForm.cs
@@ -28,28 +28,41 @@ namespace BDO_Enhacement
 
         private void cmbItemType_SelectedIndexChanged(object sender, EventArgs e)
         {
+            cmbItemName.Items.Clear();
+            cmbItemName.ResetText();
+
             //nothing selected
             if (cmbItemType.SelectedIndex == -1)
             {
-                cmbItemName.Items.Clear();
                 cmbItemName.Enabled = false;
             }
             //Accessory (Green, Blue, Yellow)
             else if (cmbItemType.SelectedIndex == 0)
             {
-                List<string> newItems = new List<string>()
+                List<string> yellowGradeItems = new List<string>()
                 {
-                    "",
+                    "Basilisk's Belt", "Black Distortion Earring", "Centaurus Belt", "Dawn Earring", "Deboreka Belt", "Deboreka Necklace", "Ethereal Earring", 
+                    "Eye of the Ruins Ring", "Forest Ronaros Ring", "Laytenn's Power Stone", "Narc Ear Accessory", "Ogre Ring", "Ominous Ring", "Orkinrad's Belt", 
+                    "Revived Lunar Necklace", "Revived River Necklace", "Ring of Cadry Guardian", "Ring of Crescent Guardian", "Serap's Necklace", 
+                    "Sicil's Necklace", "Tungrad Belt", "Tungrad Earring", "Tungrad Necklace", "Tungrad Ring", "Turo's Belt", "Valtarra Eclipsed Belt", "Vaha's Dawn"
                 };
 
-                cmbItemName.Items.Clear();
-                foreach (string item in newItems)
+                /* TODO: add blue and green grade (green should all be the same)
+                List<string> blueGradeItems = new List<string>()
+                {
+
+                };
+                */
+                foreach (string item in yellowGradeItems)
                 {
                     cmbItemName.Items.Add(item);
                 }
+                
+
                 cmbItemName.Enabled = true;
             }
             //Weapon(Green)
+            //TODO: Add green grade items (also needs to be added to Item.cs)
             else if (cmbItemType.SelectedIndex == 1)
             {
                 List<string> newItems = new List<string>()
@@ -57,7 +70,6 @@ namespace BDO_Enhacement
                     "",
                 };
 
-                cmbItemName.Items.Clear();
                 foreach (string item in newItems)
                 {
                     cmbItemName.Items.Add(item);
@@ -67,13 +79,12 @@ namespace BDO_Enhacement
             //Weapon (Blue, Yellow)
             else if (cmbItemType.SelectedIndex == 2)
             {
-                List<string> newItems = new List<string>()
+                List<string> yellowItems = new List<string>()
                 {
-                    "",
+                    "Kutum/Nouver Sub Weapon", "Dragon Slayer Awakening Weapon", "Kzarka/Offin Tett Main Weapon", "Dandelion/Artina Sol/Tring/Tute"
                 };
 
-                cmbItemName.Items.Clear();
-                foreach (string item in newItems)
+                foreach (string item in yellowItems)
                 {
                     cmbItemName.Items.Add(item);
                 }
@@ -82,16 +93,10 @@ namespace BDO_Enhacement
             //Weapon (Blackstar)
             else if (cmbItemType.SelectedIndex == 3)
             {
-                List<string> newItems = new List<string>()
-                {
-                    "",
-                };
 
-                cmbItemName.Items.Clear();
-                foreach (string item in newItems)
-                {
-                    cmbItemName.Items.Add(item);
-                }
+                cmbItemName.Items.Add("Blackstar Weapon");
+                cmbItemName.Items.Add("Godr-Ayed Weapon");
+
                 cmbItemName.Enabled = true;
             }
             //Armor (Green)
@@ -102,7 +107,6 @@ namespace BDO_Enhacement
                     "",
                 };
 
-                cmbItemName.Items.Clear();
                 foreach (string item in newItems)
                 {
                     cmbItemName.Items.Add(item);
@@ -112,13 +116,17 @@ namespace BDO_Enhacement
             //Armor (Blue, Yellow)
             else if (cmbItemType.SelectedIndex == 5)
             {
-                List<string> newItems = new List<string>()
+                List<string> yellowItems = new List<string>()
                 {
-                    "",
+                    "Red Nose's Armor", "Bheg's/Leebur's Gloves", "Muskan's Shoes", "Giath's Helmet", "Griffon's Helmet",
+                    "Urugon's Shoes", "Dim Tree Spirit's Armor"
                 };
 
-                cmbItemName.Items.Clear();
-                foreach (string item in newItems)
+
+                //add blue grade
+                cmbItemName.Items.Add("Akum/Lemoria");
+                //add yellow grade
+                foreach (string item in yellowItems)
                 {
                     cmbItemName.Items.Add(item);
                 }
@@ -129,10 +137,9 @@ namespace BDO_Enhacement
             {
                 List<string> newItems = new List<string>()
                 {
-                    "",
+                    "Blackstar Gloves", "Blackstar Shoes", "Blackstar Helmet", "Blackstar Armor"
                 };
 
-                cmbItemName.Items.Clear();
                 foreach (string item in newItems)
                 {
                     cmbItemName.Items.Add(item);
@@ -144,10 +151,9 @@ namespace BDO_Enhacement
             {
                 List<string> newItems = new List<string>()
                 {
-                    "",
+                    "Fallen God's Armor", "Labreska's Helmet"
                 };
 
-                cmbItemName.Items.Clear();
                 foreach (string item in newItems)
                 {
                     cmbItemName.Items.Add(item);
